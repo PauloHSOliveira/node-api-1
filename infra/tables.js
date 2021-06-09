9526ed3be4e41f00e4e55341ff26779989db3f13
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-06-08 23:20:25 -0300

    chore: added dates

--- infra/tables.js
+++ infra/tables.js
@@ -6,7 +6,7 @@ class Tables {
 
   createTables() {
     const sql =
-      'CREATE TABLE IF NOT EXISTS calls (id int NOT NULL AUTO_INCREMENT, customer varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, status varchar(20) NOT NULL, comments text, PRIMARY KEY (id))'
+      'CREATE TABLE IF NOT EXISTS calls (id int NOT NULL AUTO_INCREMENT, customer varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, data datetime NOT NULL, createdAt datetime NOT NULL, status varchar(20) NOT NULL, comments text, PRIMARY KEY (id))'
     this.connection.query(sql, (error) => {
       if (error) {
         console.error(error)
