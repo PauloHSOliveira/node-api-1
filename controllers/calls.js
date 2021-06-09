f7d2ab70b342d89afebe87e57626806d54f81949
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-06-08 22:18:54 -0300

    chore: create controllers

---
+++ controllers/calls.js
@@ -0,0 +1,15 @@
+const Calls = require('../models/calls')
+
+exports.getCalls = (req,res) => {
+    res.send('teste')
+}
+
+exports.createItem = async (req,res) => {
+    try {
+        const call = req.body
+        const response = await Calls.create(call)
+        res.status(200).json(response)
+    } catch (error) {
+        console.error(error)
+    }
+}
