9a7374eea2bf3852a0a8a1aff370e3363683d367
Paulo silveira <paulo.silveira@usemobile.xyz>
2021-06-08 22:19:38 -0300

    chore: create model

---
+++ models/calls.js
@@ -0,0 +1,16 @@
+const connection = require('../infra/database')
+
+class Calls {
+    create(call) {
+        const sql = 'INSERT INTO Calls SET ?'
+        connection.query(sql, call, (error,result) => {
+            if(error) {
+                console.error(error)
+            } else {
+                console.log(result)
+            }
+        })
+    }
+}
+
+module.exports = new Calls()
